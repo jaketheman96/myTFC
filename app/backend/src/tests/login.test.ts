77f97f76b90401b60456b3eacdaa4ec569021538
Jake Chien <jake_chien@outlook.com>
2022-10-27 19:14:41 -0300

    Add testes da rota /login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,86 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import Users from '../database/models/UsersModel';
+import loginMocks from './mocks/login.mocks'
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const { user1, loginSuccess, token } = loginMocks;
+
+describe('Testes da rota login', () => {
+
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(Users, "findOne")
+      .resolves(loginMocks.user1 as Users);
+  });
+
+  afterEach(() => {
+    (Users.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Testa o caso de sucesso de login', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(loginSuccess)
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.haveOwnProperty('token');
+  });
+
+  it('Testa o middleware de campos vazios do login', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: '',
+        password: user1.password,
+      })
+    expect(chaiHttpResponse.status).to.be.equal(400);
+  });
+
+  it('Testa o caso de erro com password invalido', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: user1.email,
+        password: 'xablau',
+      })
+    expect(chaiHttpResponse.status).to.be.equal(401);
+  });
+
+  it('Testa o caso de erro com email invalido', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'adm@adm.com',
+        password: user1.password,
+      })
+    expect(chaiHttpResponse.status).to.be.equal(401);
+  });
+
+  it('Testa a rota get /login/validate', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/login/validate')
+      .set('authorization', token)
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body.role).to.be.equal('admin')
+  });
+});
+
---
+++ app/backend/src/tests/mocks/login.mocks.ts
@@ -0,0 +1,16 @@
+const user1 = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+}
+
+const loginSuccess = {
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+}
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoic2VjcmV0X2FkbWluIiwiaWF0IjoxNjY2OTA3NTk4fQ.VXZuMx6LZlMJzcadL3eTGvDIdBaPg7Lew9XWsGD1EhY'
+
+export default { user1, loginSuccess, token }
